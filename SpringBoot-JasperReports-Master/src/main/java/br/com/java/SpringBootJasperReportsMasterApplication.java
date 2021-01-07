e0c9d20afb3d5a086bb203dd3c061f011c36d9cc
Diegotpereira <diegoteixeirapereira@hotmail.com>
2021-01-07 16:43:39 -0300

    criação dos arquivos java do projeto

---
+++ SpringBoot-JasperReports-Master/src/main/java/br/com/java/SpringBootJasperReportsMasterApplication.java
@@ -0,0 +1,13 @@
+package br.com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJasperReportsMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJasperReportsMasterApplication.class, args);
+	}
+
+}
